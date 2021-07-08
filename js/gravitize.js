a55e9132c1bc493189793286b41e210bc3ee7e0c
clholm <connorlholm@gmail.com>
2021-07-07 23:34:52 -0500

    begin integrating physics engine

--- js/gravitize.js
+++ js/gravitize.js
@@ -1,54 +1,52 @@
 // wrap every word of body text (paragraph and anchor) in a span
+// and assign them an incremental id for the physics engine
 function spanitize() {
   let p_arr = document.querySelectorAll("p");
   let a_arr = document.querySelectorAll("a");
   let id_counter = 0;
+  // anonymous function that iterates over node array and wraps words in <span>s
+  let span_helper = (arr, id_counter) => {
+    arr.forEach(
+      elt => {
+        // regex for matching all non-whitespace characters
+        let re = /[^\s]+/g;
+        words = elt.innerHTML.match(re);
+        // iterate over all words and wrap them in a span
+        words.forEach(
+          word => {
+            // dynamically construct a regex that matches the word with a lookahead
+            // for whitespace, periods, or a string or line end.
+            let re = new RegExp(word + "(?=(\\s|\\.|$))");
+            elt.innerHTML = elt.innerHTML.replace(re,`<span class="phys-obj phys-id-${id_counter}">${word}</span>`);
+            id_counter += 1;
+          }
+        )
+      }
+      )
+    return id_counter;
+  }
+
   if (p_arr.length < 1 && a_arr.length < 1) {
     throw "Can't find any 'paragraph' or 'anchor' elements";
   }
-  // iterate over p_arr and insert <span> tags between
+  // iterate over p_arr and a_arr and insert <span> tags between
   // words
-  p_arr.forEach(
-    elt => {
-      // console.log(elt);
-      // regex for matching all non-whitespace characters
-      let re = /[^\s]+/g;
-      words = elt.innerHTML.match(re);
-      // iterate over all words and wrap them in a span
-      words.forEach(
-        word => {
-          // dynamically constructs a regex that matches the word with a lookahead
-          // for whitespace, periods, or a string or line end.
-          let re = new RegExp(word + "(?=(\\s|\\.|$))");
-          elt.innerHTML = elt.innerHTML.replace(re,`<span class="phys-obj phys-id-${id_counter}">${word}</span>`);
-          id_counter += 1;
-        }
-      )
-    }
-  )
-  // iterate over a_arr now
-  a_arr.forEach(
-    elt => {
-      // console.log(elt);
-      // regex for matching all non-whitespace characters
-      let re = /[^\s]+/g;
-      words = elt.innerHTML.match(re);
-      // iterate over all words and wrap them in a span
-      words.forEach(
-        word => {
-          // dynamically constructs a regex that matches the word with a lookahead
-          // for whitespace, periods, or a string or line end.
-          let re = new RegExp(word + "(?=(\\s|\\.|$))");
-          elt.innerHTML = elt.innerHTML.replace(re,`<span class="phys-obj phys-id-${id_counter}">${word}</span>`);
-          id_counter += 1;
-        }
-      )
-    }
-  )
+  id_counter = span_helper(p_arr, id_counter);
+  span_helper(a_arr, id_counter);
+}
+
+// build world
+function build_world() {
+  let Engine = Matter.Engine,
+  Render = Matter.Render,
+  Runner = Matter.Runner,
+  Bodies = Matter.Bodies,
+  Composite = Matter.Composite;
+
 }
 
 // create gravity!
-function gravitize() {
+function gravitize(World) {
   // // module aliases
   // var Engine = Matter.Engine,
   // Render = Matter.Render,
